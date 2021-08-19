cae1c1f58f0067fbffee741883816559b9b32707
adedolapo <shayne.doly94@gmail.com>
2021-08-19 17:47:49 +0100

    includes json response for unvalidated user

--- utils/validateToken.js
+++ utils/validateToken.js
@@ -4,12 +4,16 @@ const validateToken = (req, res, next) => {
   const token = req.cookies.jwt
 
   if (!token) {
-    return res.send("Login please")
+    return res
+      .status(401)
+      .json({ success: false, statusCode: 401, error: "please sign in" })
   }
 
   jwt.verify(token, process.env.SECRET_KEY, (err, decodedToken) => {
     if (err) {
-      return res.send("please login")
+      return res
+        .status(401)
+        .json({ success: false, statusCode: 401, error: err })
     }
 
     req.body.userId = decodedToken.id
